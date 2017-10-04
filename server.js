52596c139a75d92a35dc3e3198cb44891e4d32e4
Ayan Dalal <adalal4@sapient.com>
2017-10-04 22:56:56 +0530

    added files, initial setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const socketIO = require('socket.io').listen(server);
+
+let users = [];
+let connections = [];
+let port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log(`server running on ${port} port`));
+
+app.use('/', express.static(path.join(__dirname, 'files')));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+socketIO.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log(`${connections.length} active connections...`);
+});
